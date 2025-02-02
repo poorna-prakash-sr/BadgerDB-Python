5d849caa3519a92934eb82616f947d8d61973cb1
Poorna Prakash SR <srprakash016@gmail.com>
2025-02-02 13:18:45 +0530

    feat: updated file structs

--- badger/__init__.py
+++
@@ -1,3 +0,0 @@
-from .badger import BadgerDB
-
-__all__ = ['BadgerDB']
--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup, find_packages
 
+with open("README.md", 'r') as f:
+    description = f.read()   
+
 setup(
     name="badgerdb-python",
-    version="1.0.0",
+    author="Poorna Prakash SR",
+    author_email="poornaprakashsr@gmail.com",
+    description = "Python wrapper for BadgerDB",
+    version="0.5.0",
     packages=find_packages(),
     install_requires=[],
     include_package_data=True,
+    long_description=description,
+    long_description_content_type='text/markdown',
     package_data={
         "badger": ["libbadger.so"]
     },
